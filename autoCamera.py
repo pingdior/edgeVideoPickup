42cfd7383361c97f238cad7f59e7d21f89f62657
pingdior <pingdior@163.com>
2022-11-11 17:10:05 +0800

    Add files via upload
    load video from edge to tx cload

---
+++ autoCamera.py
@@ -0,0 +1,54 @@
+from qcloud_vod.vod_upload_client import VodUploadClient
+from qcloud_vod.model import VodUploadRequest
+import time
+from picamera import PiCamera
+from time import sleep
+import os
+
+def getFileName():
+  localtime = time.localtime(time.time());
+  timeStr = time.strftime("%Y%m%d%H%M%S",localtime);
+  newFileName = 'wbq'+timeStr+'.h264';
+  return newFileName;
+
+def getCamera(fileName):
+  try:
+   camera = PiCamera();
+   camera.start_preview();
+   camera.start_recording(fileName);
+   sleep(30);
+   camera.stop_recording();
+   camera.stop_preview();
+  except Exception as err:
+   print(err)
+  return
+
+def convert_to_mp4(source_path):
+    HFOMAT=".h264";
+    MFOMAT=".mp4";
+    target_path = source_path;
+    target_path = target_path.replace(HFOMAT,MFOMAT);
+    print target_path;
+    print source_path;
+    cmod='MP4Box'+' -add '+source_path+' '+target_path;
+    execute_state = os.system(cmod);
+    print(cmod)
+    print(execute_state)
+    if execute_state==0:
+        return target_path;
+    else:
+        return
+
+def mp4LoadTXVod(mpFileName,vodID,vodKey,vodArea):
+ client = VodUploadClient(vodID,vodKey)
+ request = VodUploadRequest()
+ request.MediaFilePath = mpFileName;
+ request.StorageRegion = vodArea;
+ try:
+   response = client.upload(vodArea,request)
+   print(response.FileId)
+   print(response.MediaUrl)
+   return response.FileId
+ except Exception as err:
+   print(err)
+   return ""
---
+++ ctcmsconf.py
@@ -0,0 +1,4 @@
+H264_PATH='/home/pi/txvod/h264/'; 
+MP4_PATH='/home/pi/txvod/mp4/'; 
+MP4_FPS=15; 
+
