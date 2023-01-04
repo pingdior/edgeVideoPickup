9b2f8288b6b68ff6b15828cbe32928866f5be90b
pingdior <pingdior@163.com>
2023-01-04 12:35:58 +0800

    vcgencmd get_camera
    add MediaURL

--- doCreatMP4.py
+++ doCreatMP4.py
@@ -1,6 +1,9 @@
 import h264_to_mp4
 import autoCamera
 import loadTXVODParameter 
+import datetime
+MEDIOURLDIR="/home/pi/software/txiot/medioFile/"
+
 h264_to_mp4.init_video_folder();
 h264FilePath =h264_to_mp4.gen_h264_filepath();
 print h264FilePath 
@@ -10,4 +13,20 @@ print mp4FilePath
 print "--------------------------------"
 autoCamera.getCamera(h264FilePath);
 h264_to_mp4.convert_to_mp4(h264FilePath,mp4FilePath);
-autoCamera.mp4LoadTXVod(mp4FilePath,loadTXVODParameter.VODID,loadTXVODParameter.VODKEY,loadTXVODParameter.VODAREA,loadTXVODParameter.VODAPPID);
+
+mediaUrl = autoCamera.mp4LoadTXVod(mp4FilePath,loadTXVODParameter.VODID,loadTXVODParameter.VODKEY,loadTXVODParameter.VODAREA);
+print("mediaUrl:"+mediaUrl+"--------------------------------------------------------") 
+print("MEDIOURLDIR:"+MEDIOURLDIR+"-----------------------------------------------------")
+localtime = datetime.datetime.now()
+timeStr = localtime.strftime('%Y-%m-%d-%H-%M-%S')
+print(timeStr+"--------------------------------------------------------") 
+
+if mediaUrl != "":
+   fileName = MEDIOURLDIR+timeStr+".txt"
+   fw = open(fileName ,'w');
+   fw.write(mediaUrl);
+else:
+   fileName =  MEDIOURLDIR+"error"+timeStr+".txt"
+   fw = open(fileName ,'w');
+fw.close()
+
